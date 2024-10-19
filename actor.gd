8b432e4c1dd3e10b122287d12b6e93316ec70f61
Noah <nordqvist.gaming@gmail.com>
2024-10-19 17:08:05 +0200

    implement basic nav agent movement on click

--- actor.gd
+++ actor.gd
@@ -1,4 +1,32 @@
 extends CharacterBody3D
 
+
+@export var _movement_speed: float = 4.0
+
+@onready var nav_agent: NavigationAgent3D = get_node("NavigationAgent3D")
+
+
 func _ready() -> void:
 	print("Actor spawned")
+	nav_agent.velocity_computed.connect(_on_velocity_computed)
+
+
+func _physics_process(_delta: float) -> void:
+	if nav_agent.is_navigation_finished():
+		return
+
+	var next_path_position: Vector3 = nav_agent.get_next_path_position()
+	var new_velocity: Vector3 = global_position.direction_to(next_path_position) * _movement_speed
+	if nav_agent.avoidance_enabled:
+		nav_agent.set_velocity(new_velocity)
+	else:
+		_on_velocity_computed(new_velocity)
+
+
+func set_movement_target(_target_position: Vector3) -> void:
+	nav_agent.target_position = _target_position
+
+
+func _on_velocity_computed(safe_velocity: Vector3) -> void:
+	velocity = safe_velocity
+	move_and_slide()
--- game_manager.gd
+++ game_manager.gd
@@ -1,6 +1,29 @@
 extends Node3D
 
 
+@export var _camera: Camera3D
+@export var _actor: CharacterBody3D
+@export var _indicator: MeshInstance3D
+
+
 func _input(event: InputEvent) -> void:
-	if (event is InputEventMouseButton):
-		pass
+	if event.is_action_pressed("right_click"):
+		var origin: Vector3 = _camera.project_ray_origin(event.position)
+		var direction: Vector3  = _camera.project_ray_normal(event.position)
+		var end: Vector3 = origin + direction * 1000
+		var ray_query = PhysicsRayQueryParameters3D.new()
+		ray_query.from = origin
+		ray_query.to = end
+		ray_query.collide_with_areas = true
+
+		var state: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
+
+		var intersection: Dictionary = state.intersect_ray(ray_query)
+		if intersection.size() > 0:
+			if intersection.collider.is_in_group("ground"):
+				print("hit ground")
+				var target_position: Vector3 = intersection["position"] as Vector3
+				_actor.set_movement_target(target_position)
+				_indicator.global_position = target_position
+			elif intersection.collider.is_in_group("actors"):
+				print("hit actor")
